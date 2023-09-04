00386cc13d48ebc7a90cc85af2d7a30ddb60947f
Leino <leinovladimir@gmail.com>
2023-09-04 16:53:43 +0400

    init clear next.js

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+
+
+
+export const metadata: Metadata = {
+  title: 'Ловим и уничтожаем астероиды',
+  description: 'Приложение, в котором можно уничтожить астероид',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
